ef03cccaf83697b86e6a0ba6215b79457d1e4b32
hunterperlman <62340670+hunterperlman@users.noreply.github.com>
2020-04-02 03:12:09 -0400

    type your commit message here

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+//-- Auto Generate Angular Comment --
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
+// Routing file for connecting pages
+
+// package imports
 import { Routes } from "@angular/router";
 
+// component imports
 import { EmployeeAddComponent } from "./app/employee-add/employee-add.component";
 import { EmployeeSetComponent } from "./app/employee-set/employee-set.component";
 
+// maintains the routs on the web page
 export const routes : Routes = [
     { path : "home", component : EmployeeAddComponent },
     { path : "employee-set", component : EmployeeSetComponent },
